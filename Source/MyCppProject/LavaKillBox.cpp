6e46db69eeba684c2fce38203566e1c70b8d8338
Théo <theo.boldireff@edu.devinci.fr>
2020-11-17 16:19:21 +0100

    Kill Lava cpp (WIP)

---
+++ Source/MyCppProject/LavaKillBox.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+/*
+#define print(text) if(GEngine) GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::Green, text)
+#define printf(text, fstring) if(GEngine) GEngine->AddOnScreenDebugMessage(-1, 1.5, Fcolor::Green, FString::Printf(Text(text), fstring))
+
+#include "LavaKillBox.h"
+#include "DrawDebugHelpers.h"
+
+ALavaKillBox::ALavaKillBox() 
+{
+	OnActorBeginOverlap.AddDynamic(this, &ALavaKillBox::OnOverlapBegin);
+	OnActorEndOverlap.AddDynamic(this, &ALavaKillBox::OnOverlapEnd);
+
+}
+
+void ALavaKillBox::BeginPlay() 
+{
+	Super::BeginPlay();
+
+	DrawDebugBox(GetWorld(), GetActorLocation(), GetComponentsBoundingBox().GetExtent(), FColor::Purple, true, -1, 0, 5);
+}
+
+void ALavaKillBox::OnOverlapBegin(class AActor* OverlappedActor, class AActor* OtherActor)
+{
+	if (OtherActor && (OtherActor != this))
+	{
+		print("Overlap Begin");
+		printf("Overlapped Actor = %S", OverlappedActor->GetName());
+	}
+}
+
+void ALavaKillBox::OnOverlapEnd(class AActor* OverlappedActor, class AActor* OtherActor)
+{
+	if (OtherActor && (OtherActor != this))
+	{
+		print("Overlap Ended");
+		printf("Overlapped Actor = %S", *OtherActor->GetName());
+	}
+}*/
---
+++ Source/MyCppProject/LavaKillBox.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+/*
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerBox.h"
+#include "LavaKillBox.generated.h"
+
+
+UCLASS()
+class MYCPPPROJECT_API ALavaKillBox : public ATriggerBox
+{
+	GENERATED_BODY()
+	
+protected:
+
+	virtual void BeginPlay() override;
+
+public:
+
+	ALavaKillBox();
+
+	UFUNCTION()
+		void OnOverlapBegin(class AActor* OverlappedActor, class AActor* OtherActor);
+
+	UFUNCTION()
+		void OnOverlapEnd(class AActor* OverlappedActor, class AActor* OtherActor);
+};
+*/
